ecee677da377ffccd500546c6e9386c691c0acd6
Samuel.Cai <samuel.cai@ceosoftcenters.com>
2017-10-04 20:18:11 -0700

    Add State and Strategy Design Pattern

---
+++ src/com/pattern/behavioral/state/State.java
@@ -0,0 +1,5 @@
+package com.pattern.behavioral.state;
+
+public interface State {
+	public void doAction();
+}
---
+++ src/com/pattern/behavioral/strategy/PaymentStrategy.java
@@ -0,0 +1,5 @@
+package com.pattern.behavioral.strategy;
+
+public interface PaymentStrategy {
+	public void pay(int amount);
+}
